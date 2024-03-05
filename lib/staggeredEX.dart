3e6a3e77595c40b3075dfc40a76729e79b28d216
sanjaykt <sanjaysatheesh1346@gmail.com>
2024-03-05 11:42:25 +0530

    commit

--- lib/staggeredEX.dart
+++ lib/staggeredEX.dart
@@ -1,57 +1,117 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-void main(){
-  runApp(MaterialApp(home: stagex(),));
+
+void main() {
+  runApp(MaterialApp(
+    home: stagex(),
+  ));
 }
-class stagex extends StatelessWidget{
+
+class stagex extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("staggeredrecylerview"),titleTextStyle: TextStyle(color: Colors.blue),),backgroundColor: Colors.blueGrey,
-    body: StaggeredGrid.count(crossAxisCount: 4,
-    mainAxisSpacing: 4,
-    crossAxisSpacing: 4,
-    children: const [
-    StaggeredGridTile.count(crossAxisCellCount: 2, mainAxisCellCount: 1,
-    child: Card(
-    child: Column(
-    children: [
-    Image(image: AssetImage("assets/images/pexels-jonathan-meyer-612999.jpg"),height: 100,width: double.infinity,),
-      Text("havasu falls"),
-    ],
-    ),
-    )),
-    StaggeredGridTile.count(crossAxisCellCount: 2, mainAxisCellCount: 3,
-    child: Card(
-    child: Column(
-    children: [
-    Image(image: AssetImage("assets/images/pexels-mateusz-walendzik-15771577.jpg"),fit: BoxFit.contain),
-    Text("trondheim"),
-    ],
-    ),
-    )),
-    StaggeredGridTile.count(crossAxisCellCount: 2, mainAxisCellCount: 3,
-    child: Card(
-    child: Column(
-    children: [
-    Padding(
-    padding: const EdgeInsets.only(bottom: 30),
-      child:Image(image: AssetImage("assets/images/pexels-nuno-veludo-3558637.jpg"),fit: BoxFit.contain),
-
-    )],
-    ),
-    )),
-      StaggeredGridTile.count(crossAxisCellCount: 2, mainAxisCellCount: 2,
-        child: Card(
-          child: Column(
-            children: [
-              Image(image: AssetImage("assets/images/pexels-paige-thompson-19841515.jpg"),fit: BoxFit.cover),
-              Text("rocky mountain national park"),
-            ],
-          ),
-        ))
-    ])
-    );
+        appBar: AppBar(
+          title: Text("staggeredrecylerview"),
+          titleTextStyle: TextStyle(color: Colors.blue),
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: SingleChildScrollView(
+          child: StaggeredGrid.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 4,
+              crossAxisSpacing: 4,
+              children:  [
+                StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 1,
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Container(
+                              height: 250,
+                              width: double.infinity,
+                              child: Image(
+                                image: AssetImage(
+                                    "assets/images/pexels-jonathan-meyer-612999.jpg"),
+                                height: 80,
+                                width: 230,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Text("havasu falls"),
+                        ],
+                      ),
+                    )),
+                StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 2,
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              height: 550,
+                              width: double.infinity,
+                              child: Image(
+                                  image: AssetImage(
+                                      "assets/images/pexels-mateusz-walendzik-15771577.jpg"),
+                                  height: 1200,
+                                  width: 250,
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                          Text("trondheim"),
+                        ],
+                      ),
+                    )),
+                StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 2,
+                    child: Card(
+                        child: Column(
+                            children: [
+                              Padding(padding: const EdgeInsets.all(10),
+                              child: Container(
+                                height: 550,
+                                    width: double.infinity,
+                              child: Image(
+                          image: AssetImage(
+                              "assets/images/pexels-paige-thompson-19841515.jpg"),
+                                  height: 1200,
+                                  width: 250,
+                                  fit: BoxFit.cover),)),
+                      Text("rocky mountain national park"),
+                    ]))),
+                StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 1,
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              height: 250,
+                              width: double.infinity,
+                              child: Image(
+                                  image: AssetImage(
+                                      "assets/images/pexels-nout-gons-378570.jpg"),
+                                  height: 80,
+                                  width: 230,
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                          Text("portugal"),
+                        ],
+                      ),
+                    ))
+              ]),
+        ));
   }
 }
